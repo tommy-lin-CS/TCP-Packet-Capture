4a6b93e96be049012db1ae4c8bb9192cbed4d01d
Tommy Lin <69088385+tommy-lin-CS@users.noreply.github.com>
2022-11-02 01:26:47 -0400

    Added Congestion Window Algorithm, Put .txt in README

--- README.md
+++ README.md
@@ -7,5 +7,96 @@
     1. In our assignment, we will be running python analysis_pcap_tcp.py assignment2.pcap
 
 **Please make sure you have Python and dpkt library installed on your computer before attempting to run the steps above. You may find the follow instructions to install python and dpkt here:**
-https://www.python.org/downloads/
-https://dpkt.readthedocs.io/en/latest/installation.html
+* https://www.python.org/downloads/
+* https://dpkt.readthedocs.io/en/latest/installation.html
+
+
+## Output (assignment2.pcap)
+```
+----------------------------------------------------------------------------------------------------
+|  TCP FLOW  |         TCP TUPLE (SRCPORT, SRCIP, DESTPORT, DESTIP)         |      THROUHGPUT      |
+----------------------------------------------------------------------------------------------------
+|      1     |        (43498, '130.245.145.12', 80, '128.208.2.198')        |  5342630.8964322135  |
+----------------------------------------------------------------------------------------------------
+|      2     |        (43500, '130.245.145.12', 80, '128.208.2.198')        |  1268045.245854818  |
+----------------------------------------------------------------------------------------------------
+|      3     |        (43502, '130.245.145.12', 80, '128.208.2.198')        |  1626650.4791975326  |
+---------------------------------------------------------------------------------------------------- 
+
+
+
+
+		FIRST TWO TRANSACTIONS AFTER CONNECTION SETUP FOR TCP FLOW 1
+----------------------------------------------------------------------------------------------------
+|           SOURCE -> DESTINATION          | (SEQ NUMBER, ACK NUMBER) |    RECEIVE WINDOWS SIZE   |
+----------------------------------------------------------------------------------------------------
+| 130.245.145.12:43498 -> 128.208.2.198:80 | (705669103, 1921750144) |             3             |
+| 130.245.145.12:80 -> 128.208.2.198:43498 | (1921750144, 705669127) |             3             |
+----------------------------------------------------------------------------------------------------
+| 128.208.2.198:43498 -> 130.245.145.12:80 | (705669127, 1921750144) |             3             |
+| 128.208.2.198:80 -> 130.245.145.12:43498 | (1921750144, 705670575) |             3             |
+----------------------------------------------------------------------------------------------------
+------------------------------------------------------------
+|TRIPLE DUPLICATE ACKS| TIMEOUTS |    FIRST 3 CWND SIZE   |
+------------------------------------------------------------
+|          2          |    1     |       14, 18, 41       |
+------------------------------------------------------------
+
+
+
+		FIRST TWO TRANSACTIONS AFTER CONNECTION SETUP FOR TCP FLOW 2
+----------------------------------------------------------------------------------------------------
+|           SOURCE -> DESTINATION          | (SEQ NUMBER, ACK NUMBER) |    RECEIVE WINDOWS SIZE   |
+----------------------------------------------------------------------------------------------------
+| 130.245.145.12:43500 -> 128.208.2.198:80 | (3636173852, 2335809728) |             3             |
+| 130.245.145.12:80 -> 128.208.2.198:43500 | (2335809728, 3636173876) |             3             |
+----------------------------------------------------------------------------------------------------
+| 128.208.2.198:43500 -> 130.245.145.12:80 | (3636173876, 2335809728) |             3             |
+| 128.208.2.198:80 -> 130.245.145.12:43500 | (2335809728, 3636175324) |             3             |
+----------------------------------------------------------------------------------------------------
+------------------------------------------------------------
+|TRIPLE DUPLICATE ACKS| TIMEOUTS |    FIRST 3 CWND SIZE   |
+------------------------------------------------------------
+|          4          |    90    |       10, 20, 33       |
+------------------------------------------------------------
+
+
+
+		FIRST TWO TRANSACTIONS AFTER CONNECTION SETUP FOR TCP FLOW 3
+----------------------------------------------------------------------------------------------------
+|           SOURCE -> DESTINATION          | (SEQ NUMBER, ACK NUMBER) |    RECEIVE WINDOWS SIZE   |
+----------------------------------------------------------------------------------------------------
+| 130.245.145.12:43502 -> 128.208.2.198:80 | (2558634630, 3429921723) |             3             |
+| 130.245.145.12:80 -> 128.208.2.198:43502 | (3429921723, 2558634654) |             3             |
+----------------------------------------------------------------------------------------------------
+| 128.208.2.198:43502 -> 130.245.145.12:80 | (2558634654, 3429921723) |             3             |
+| 128.208.2.198:80 -> 130.245.145.12:43502 | (3429921723, 2558636102) |             3             |
+----------------------------------------------------------------------------------------------------
+------------------------------------------------------------
+|TRIPLE DUPLICATE ACKS| TIMEOUTS |    FIRST 3 CWND SIZE   |
+------------------------------------------------------------
+|          0          |    0    |       20, 43, 44       |
+------------------------------------------------------------
+```
+
+
+## Explanation
+### Part A
+a. The code loops through the entire TCP file. Starting from the SYN flag, if the source port, source IP address, destination port, and destination IP address has already been recorded, we do not record it again. If not, we append it to our TCP Flow tuple. The TCP Flow tuple should include the (source port, source IP address, destination port, destination IP address) by format.
+
+b. To get the first two transactions of the flow, I used the TCP Flow tuple I found in Part A.a. Basically, I look through the list for each TCP Flow, and I get the first and second transactions sent from sender to receiver and from receiver to sender. I record each of their sequence number -> acknowledgment number and their receive window size.
+
+c. To get the throughput of each TCP Flow, I add the length (data) of each TCP packet in that flow. In addition, I would also check for SYN_FLAG and mark the timestamp of it and check for FIN_FLAG and mark the timestamp of it. After that, to get the period, I take the timestamp of the FIN_FLAG subtract the timestamp of the SYN_FLAG. Then I calculate the throughput for that flow by dividing the computed length (data) of each TCP packet by the period.
+
+### Part B
+a. To get the first three congestion windows, I first calculated the RTT for each flow. To calculate the RTT for each flow, I subtracted the last timestamp of the handshake by the first timestamp of the handshake. Then, I counted how many valid packets are between first TCP packet timestamp (min_ts) in that flow to the first TCP packet timestamp + my estimated RTT (max_ts). To get the next congestion window, I set the min_ts to my last max_ts and set the max_ts to my new min_ts + my estimated RTT. 
+The rate that congestion window grows is due to TCP slow start. The congestion window doubles per RTT until it hits the slow start threshold then it will start increasingly linearly. This can be shown in our third flow which the congestion window changes from 20 -> 43 -> 44 (doubles, then linear).
+
+b. To get the triple duplicate acknowledgments and timeouts, I first loop through each TCP Flow to get the sequence numbers and acknowledgment numbers of each packet in each flow after the three-way handshake. Then I use the Python Counter which is part of the Collections to count the number of times a sequence number (saved it to duplicate_seq_check) or acknowledgment number (saved it to triple_dup_ack) appears from the sender to receiver and vice versa. Then I found the intersection (the alikes) of both of the array and saved it to a variable, intersection. Then I loop through each element in the intersection and check if there are packets where the first duplicate acknowledgment is after the retransmited packet count and I save that occurrance to a variable, pkt_out_order. Finally, to the number of triple duplicate acknowledgment, I take the length of the intersection subtract the pkt_out_order. To get the number of timeouts, I take the length of the triple duplicate knowledgments (when the number of acknowledgments is greater than 3) subtract the number of triple duplicate acknowledgments.
+
+
+## Credits
+https://jon.oberheide.org/blog/2008/10/15/dpkt-tutorial-2-parsing-a-pcap-file/
+https://www.geeksforgeeks.org/python-counter-objects-elements/
+https://dpkt.readthedocs.io/en/latest/examples.html#jon-oberheide-s-examples
+https://www.howtouselinux.com/post/tcp-flags
--- analysis_pcap_tcp.py
+++ analysis_pcap_tcp.py
@@ -1,5 +1,4 @@
 from collections import Counter
-import socket
 import dpkt
 import sys
 
@@ -19,7 +18,6 @@ def tcpFlows(file_name):
         ip = eth.data
         tcp = ip.data
         
-        # fin_flag = ( tcp.flags & 0x01 ) != 0
         syn_flag = ( tcp.flags & 0x02 ) != 0
 
         sport = tcp.sport # TCP Source Port #
@@ -30,8 +28,9 @@ def tcpFlows(file_name):
             
             # check for tcp flows in the tuple already
             for tcp_flow in tcp_flows: 
-                if (sport == tcp_flow[0] or sport == tcp_flow[2]) and (dport == tcp_flow[0] or dport == tcp_flow[2]):
-                    existBool = True
+                if (sport == tcp_flow[0] or sport == tcp_flow[2]):
+                    if (dport == tcp_flow[0] or dport == tcp_flow[2]):
+                        existBool = True
             # if does not exist in tuple, append to tuple
             if not existBool:
                 tcp_flows.append((sport, sender, dport, receiver))
@@ -49,7 +48,7 @@ def getTransactionsAndThroughput(file_name, tcp_flows):
     
     flow_count_trans = [0] * (length_of_tcp_flows) # initialize array, used to get transactions
 
-    first_two_transactions = [[] for _ in range((length_of_tcp_flows) * 2)]
+    first_two_transactions = [[] for x in range((length_of_tcp_flows) * 2)]
        
     temp = [-1] * (length_of_tcp_flows) # temp used to store length tcp info of flows used to get throughput and period
     first = [-1] * (length_of_tcp_flows) # the first pkt timestamp for each tcp flow
@@ -59,11 +58,9 @@ def getTransactionsAndThroughput(file_name, tcp_flows):
         eth = dpkt.ethernet.Ethernet(buf)
         ip = eth.data
         tcp = ip.data
-        srcip = socket.inet_ntoa(ip.src)
-        destip = socket.inet_ntoa(ip.dst)
 
-        fin_flag = ( tcp.flags & dpkt.tcp.TH_FIN ) != 0
-        syn_flag = ( tcp.flags & dpkt.tcp.TH_SYN ) != 0
+        fin_flag = ( tcp.flags & 0x01 ) != 0
+        syn_flag = ( tcp.flags & 0x02 ) != 0
        
         sport = tcp.sport
         dport = tcp.dport
@@ -75,14 +72,14 @@ def getTransactionsAndThroughput(file_name, tcp_flows):
             else:
                 if tcp_flow[0] == sport and tcp_flow[2] == dport:
                     if len(first_two_transactions[2 * i]) == 0: 
-                        first_two_transactions[2 * i].append(f'| {srcip}:{sport} -> {destip}:{dport} | ({tcp.seq}, {tcp.ack}) |             {tcp.win}             |')
+                        first_two_transactions[2 * i].append(f'| {sender}:{sport} -> {receiver}:{dport} | ({tcp.seq}, {tcp.ack}) |             {tcp.win}             |')
                     elif len(first_two_transactions[2 * i]) == 1:
-                        first_two_transactions[2 * i].append(f'| {srcip}:{sport} -> {destip}:{dport} | ({tcp.seq}, {tcp.ack}) |             {tcp.win}             |')
+                        first_two_transactions[2 * i].append(f'| {receiver}:{sport} -> {sender}:{dport} | ({tcp.seq}, {tcp.ack}) |             {tcp.win}             |')
                 elif tcp_flow[0] == dport and tcp_flow[2] == sport:
                     if len(first_two_transactions[(2 * i) + 1]) == 0:
-                        first_two_transactions[(2 * i) + 1].append(f'| {srcip}:{sport} -> {destip}:{dport} | ({tcp.seq}, {tcp.ack}) |             {tcp.win}             |')
+                        first_two_transactions[(2 * i) + 1].append(f'| {sender}:{sport} -> {receiver}:{dport} | ({tcp.seq}, {tcp.ack}) |             {tcp.win}             |')
                     elif len(first_two_transactions[(2 * i) + 1]) == 1:
-                        first_two_transactions[(2 * i) + 1].append(f'| {srcip}:{sport} -> {destip}:{dport} | ({tcp.seq}, {tcp.ack}) |             {tcp.win}             |')
+                        first_two_transactions[(2 * i) + 1].append(f'| {receiver}:{sport} -> {sender}:{dport} | ({tcp.seq}, {tcp.ack}) |             {tcp.win}             |')
 
         # GET THROUGHPUT (Part A.c)
         for j, tcp_flow in enumerate(tcp_flows):
@@ -101,19 +98,66 @@ def getTransactionsAndThroughput(file_name, tcp_flows):
     
     return first_two_transactions, throughputs
 
-# def congestionWindow(file_name, tcp_flows):
-#     file_name = open(file_name, "rb")
-#     pcap = dpkt.pcap.Reader(file_name)
+# Getting Congestion Windows for each flow
+def congestionWindow(file_name, tcp_flows):
+    file_name = open(file_name, "rb")
+    pcap = dpkt.pcap.Reader(file_name)
 
-#     for ts, buf in pcap:
-#         eth = dpkt.ethernet.Ethernet(buf)
-#         ip = eth.data
-#         tcp = ip.data
+    rtt = [] # array that stores RTT
+    handshakes = [[] for _ in range(len(tcp_flows))] # allocates space for the first three handshakes
+    handshake_counter = [0 for _ in range(len(tcp_flows))] # counter for the number of handshakes up to 3
+    tcp_packet = [[] for _ in range(len(tcp_flows))] # tcp flow packets
 
-#         sport = tcp.sport
-#         dport = tcp.dport
+    # general stuff
+    for ts, buf in pcap:
+        eth = dpkt.ethernet.Ethernet(buf)
+        ip = eth.data
+        tcp = ip.data
 
-#         # for i, tcp_flow in enumerate(tcp_flows):
+        sport = tcp.sport
+        dport = tcp.dport
+
+    # first, find the RTT
+        for i, tcp_flow in enumerate(tcp_flows):
+            if ((sport == tcp_flow[0] and dport == tcp_flow[2]) or (sport == tcp_flow[2] and dport == tcp_flow[0])): # on three-way handshake
+                if(handshake_counter[i] < 3):
+                    handshake_counter[i] = handshake_counter[i] + 1
+                    handshakes[i].append(ts)
+                tcp_packet[i].append((ts, tcp))
+    
+    # get cwnd time
+    cwnd_result = []
+    for j, flow in enumerate(tcp_flows):
+        rtt.append(handshakes[j][2] - handshakes[j][0]) # append rtt
+        min_ts = tcp_packet[j][3][0]
+        max_ts = tcp_packet[j][3][0] + rtt[j]
+        ceiling = tcp_packet[j][len(tcp_packet[j]) - 1][0]
+        
+        i = 0
+        while i < 3:
+            packet_counter = 0
+            if max_ts <= ceiling:
+                for pkt in tcp_packet[j]:
+                    if min_ts <= pkt[0] < max_ts:
+                        if pkt[1].sport == flow[0] and pkt[1].dport == flow[2]:
+                            packet_counter = packet_counter + 1
+                cwnd_result.append(packet_counter)
+                min_ts = max_ts
+                max_ts = max_ts + rtt[j]
+            else: # max_ts > ceiling
+                for pkt in tcp_packet[j]:
+                    if min_ts <= pkt[0] <= ceiling:
+                        if pkt[1].sport == flow[0] and pkt[1].dport == flow[2]:
+                            packet_counter = packet_counter + 1
+                cwnd_result.append(packet_counter)
+                break
+            i += 1
+        # The congestion window grows due to TCP slow start. The congestion window doubles per RTT until 
+        # it hits the slow start threshold then it will start increasingly linearly. This can be shown in our
+        # third flow which the congestion window changes from 20 -> 43 -> 44 (doubles, then linear).
+
+    return cwnd_result
+        
 
 # Getting # of Triple Acks and Timeouts
 def retransmissions(file_name, tcp_flows):
@@ -124,35 +168,39 @@ def retransmissions(file_name, tcp_flows):
     tcp_packets = []
 
     file_name = open(file_name, "rb")
-    for _, buf in dpkt.pcap.Reader(file_name):
+    pcap = dpkt.pcap.Reader(file_name)
+    for _, buf in pcap:
         eth = dpkt.ethernet.Ethernet(buf)
         ip = eth.data
         tcp = ip.data
 
-        tcp_packets.append(tcp) # allocating tcp of each packet
+        tcp_packets.append(tcp) # allocating tcp packet
 
     for tcp_flow in tcp_flows:
         from_sender = [] # packet from sender -> receiver
         from_receiver = [] # packet from receiver -> sender
         sender_port = tcp_flow[0]
         receiver_port = tcp_flow[2]
-        handshake = 1
-        
+        handshake = 0 # used to tell if handshake period has crossed
+        index = 0
         for pkt in tcp_packets:
-            if handshake < 4 and ((pkt.sport == sender_port and pkt.dport == receiver_port) or (pkt.sport == receiver_port and pkt.dport == sender_port)): # bypass three-way handshake
-                handshake += 1
-                continue
+            if handshake < 3: # bypass three-way handshake
+                if((pkt.sport == sender_port and pkt.dport == receiver_port) or (pkt.sport == receiver_port and pkt.dport == sender_port)):
+                    handshake += 1
+                    continue
             if(pkt.sport == sender_port and pkt.dport == receiver_port):
-                from_sender.append((pkt.seq, pkt.ack))
+                from_sender.append((pkt.seq, pkt.ack, index))
+                index = index + 1
             elif(pkt.sport == receiver_port and pkt.dport == sender_port):
-                from_receiver.append((pkt.seq, pkt.ack))
-            
+                from_receiver.append((pkt.seq, pkt.ack, index))
+                index = index + 1
+
+        # Using Python Collections Library Counter to count the sequence and acknowledgment number occurrances
         sender_receiver_count = Counter([packet[0] for packet in from_sender])
         receiver_sender_count = Counter([packet[1] for packet in from_receiver])
 
-        duplicate_seq_check = [seq for seq, count in sender_receiver_count.items() if count > 1]
-        triple_dup_check = [ack for ack, count in receiver_sender_count.items() if count > 3]
-        print(triple_dup_check)
+        duplicate_seq_check = [seq for seq, count in sender_receiver_count.items() if count > 1] # filter out seq that appears more than 1
+        triple_dup_check = [ack for ack, count in receiver_sender_count.items() if count > 3] # filter out ack that appears more than 3
         intersection = list(set(triple_dup_check) & set(duplicate_seq_check)) # intersection of the set of two arrays
 
         length_of_intersection = len(intersection) 
@@ -163,22 +211,22 @@ def retransmissions(file_name, tcp_flows):
             first_dup = 0
             
             # From Receiver -> Sender
-            count = 0 # reset
+            count = 0 # count packet
             for pkt in from_receiver:
                 if pkt[1] == each:
                     count = count + 1
                 if count == 2:
-                    first_dup = pkt[0]
+                    first_dup = pkt[2] # setting the index of packet to the first duplicate we come across
                     break
 
             # From Sender -> Receiver
-            count = 0 
+            count = 0 # count packet
             for pkt in from_sender:
                 if pkt[0] == each:
-                    count = count + 1
+                    count = count + 1 
                 if count == 2:
-                    if first_dup > pkt[0]: 
-                        pkt_out_order += 1
+                    if first_dup > pkt[2]: # check to see if the first duplicate (index of packet) we found earlier is greater than the index of the packet that sent from sender -> receiver
+                        pkt_out_order += 1 # if so, increment this number to delete later
                     break
  
         triple_dup = length_of_intersection - pkt_out_order
@@ -187,15 +235,12 @@ def retransmissions(file_name, tcp_flows):
 
     return result
 
-
-if __name__=="__main__":
-    file_name = sys.argv[1] # fileName argument when program is being ran (e.g. python analysis_pcap_tcp.py *assignment2.pcap*)
+def main():
     flows = tcpFlows(file_name) # returns the number of flows in the pcap file (Part A.a)
-    
     transaction_output, throughput_output = getTransactionsAndThroughput(file_name, flows) # Part A.b&c
-    # congestionWindow(file_name, flows) 
-    retransmissions_output = retransmissions(file_name, flows)
-    print(retransmissions_output)
+    congestion_window_output = congestionWindow(file_name, flows) # Part B.a
+    retransmissions_output = retransmissions(file_name, flows) # Part B.b
+
     # PRINTING STARTS
     print("-" * 100)
 
@@ -205,10 +250,10 @@ if __name__=="__main__":
         print("-" * 100)
         print(f"|      {index}     |        {flows[index - 1]}        |  {throughput_output[index - 1]}  |")
 
-    print("-" * 100, "\n\n")
+    print("-" * 100, "\n")
 
     for index, tcp_flow in enumerate(flows, 1):
-        print("\t\tFIRST TWO TRANSACTIONS AFTER CONNECTION SETUP FOR TCP FLOW", index)
+        print("\n\n\n\t\tFIRST TWO TRANSACTIONS AFTER CONNECTION SETUP FOR TCP FLOW", index)
         print("-" * 100)
         print("|           SOURCE -> DESTINATION          | (SEQ NUMBER, ACK NUMBER) |    RECEIVE WINDOWS SIZE   |")
         print("-" * 100)
@@ -216,13 +261,15 @@ if __name__=="__main__":
                 print(transaction_output[(2 * (index - 1))][i]) # transaction from sender to receiver 
                 print(transaction_output[(2 * (index - 1)) + 1][i]) # transaction from receiver to sender
                 print("-" * 100)
-        print("\n# of times retransmission occurred due to triple duplicate acknowledgments:", retransmissions_output[((index - 1))][0])
-        print("# of times retransmission occurred due to timeouts:", retransmissions_output[((index - 1))][1])
-        print("\n")
+        first_cwnd = congestion_window_output[(3 * (index - 1))] # transaction from sender to receiver 
+        second_cwnd = congestion_window_output[(3 * (index - 1)) + 1] # transaction from receiver to sender
+        third_cwnd = congestion_window_output[(3 * (index - 1)) + 2] # transaction from receiver to sender
+        print("-" * 60)
+        print("|TRIPLE DUPLICATE ACKS| TIMEOUTS |    FIRST 3 CWND SIZE   |")
+        print("-" * 60)
+        print(f"|          {retransmissions_output[((index - 1))][0]}          |    {retransmissions_output[((index - 1))][1]}    |       {first_cwnd}, {second_cwnd}, {third_cwnd}       |")
+        print("-" * 60)
 
-
-# CREDITS
-# https://jon.oberheide.org/blog/2008/10/15/dpkt-tutorial-2-parsing-a-pcap-file/
-# https://www.geeksforgeeks.org/python-counter-objects-elements/
-# https://dpkt.readthedocs.io/en/latest/examples.html#jon-oberheide-s-examples
-# https://www.howtouselinux.com/post/tcp-flags
+if __name__=="__main__":
+    file_name = sys.argv[1] # fileName argument when program is being ran (e.g. python analysis_pcap_tcp.py *assignment2.pcap*)
+    main() # main method call
